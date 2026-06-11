0dabce5879433b997d711eb97d0433a263d7101b
Adi Purnama <imadeadipurnamayasa@gmail.com>
2026-06-11 13:47:52 +0800

    11/06/2026 Adi Update v0.1.2

--- config/app.php
+++ config/app.php
@@ -1,14 +1,7 @@
 <?php
-$base_url = (
-    (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
-    ? "https://"
-    : "http://"
-) . $_SERVER['HTTP_HOST'];
-
-$base_url .= dirname($_SERVER['SCRIPT_NAME']);
 
 define('APP_NAME',    'YummySpot');
-define('APP_URL',     $base_url);
+define('APP_URL',     'https://yummy.caro-studio.com/');
 define('APP_VERSION', '1.0.0');
 define('UPLOAD_PATH', __DIR__ . '/../uploads/');
 define('UPLOAD_URL',  APP_URL . '/uploads/');
