c13e0bdaa957abd5df041309f6377ceba6bd9645
Adi Purnama <imadeadipurnamayasa@gmail.com>
2026-06-11 13:29:30 +0800

    11/06/2026 Adi Update v0.1.0

--- includes/header.php
+++ includes/header.php
@@ -23,6 +23,7 @@ $pal = $user ? $palettes[($user['id']-1) % count($palettes)] : $palettes[0];
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <meta name="csrf" content="<?= csrfToken() ?>">
 <title><?= e($pageTitle ?? APP_NAME) ?></title>
+<link rel="icon" href="<?= APP_URL ?>/assets/img/favicon.png">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="stylesheet" href="<?= APP_URL ?>/assets/css/app.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
